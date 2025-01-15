41b5277535b99b64bb31d24f33dfc65837ab50a0
karenxpn <karenmirakyan@gmail.com>
2025-01-15 14:22:46 +0400

    base model timestamp created

---
+++ models/base.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime
+from sqlalchemy.orm import declarative_base
+from main import session
+
+Model = declarative_base()
+Model.query = session.query_property()
+
+class TimeStampedModel(Model):
+    __abstract__ = True
+
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
