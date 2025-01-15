986f3db4bac1ce443d0d2887c4f4b7a510e48630
karenxpn <karenmirakyan@gmail.com>
2025-01-15 16:45:50 +0400

    update db records

---
+++ queries/updates.py
@@ -0,0 +1,22 @@
+from main import session
+from models.user import Preference, User
+#
+# user_preference = (
+#     Preference.query
+#     .join(Preference.user)
+#     .filter(User.email == 'johndoe@gmail.com')
+#     .first()
+# )
+#
+# user_preference.currency = 'GBP'
+# session.commit()
+#
+# print(user_preference.currency)
+#
+
+user = User.query \
+        .filter(User.first_name == "John") \
+        .filter(User.last_name == "Doe") \
+        .update({ 'email': 'johndoe@hotmail.com'})
+session.commit()
+
